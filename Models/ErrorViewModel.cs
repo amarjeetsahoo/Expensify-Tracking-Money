844f1eedddc5c61069a13e6f8d2b8ad7cc581457
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2024-07-27 11:00:56 +0530

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Expensify.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
