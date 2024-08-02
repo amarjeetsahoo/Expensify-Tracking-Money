bf47f1ad69380c9e74fc5ed6acb321804d58f0c3
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2024-08-02 09:10:21 +0530

    basic setup-dark themed

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -47,7 +47,7 @@ namespace Expensify.Controllers
         // GET: Category/Create
         public IActionResult Create()
         {
-            return View();
+            return View(new Category());
         }
 
         // POST: Category/Create
