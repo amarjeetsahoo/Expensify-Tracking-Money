9b9b4afb84944e693db796b169df5fd28e3745ac
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2024-08-03 13:19:44 +0530

    CategoryIndex Page

--- Models/Category.cs
+++ Models/Category.cs
@@ -17,5 +17,14 @@ namespace Expensify.Models
 
         [Column(TypeName = "nvarchar(10)")]
         public string Type { get; set; } = "Expense";
+
+        [NotMapped]
+        public string? TitleWithIcon
+        {
+            get
+            {
+                return this.Icon + " " + this.Title;
+            }
+        }
     }
 }
